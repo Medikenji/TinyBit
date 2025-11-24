e8daf9d6ec0bbeff2dade23d939002216b5bd897
thatalloguy <allosander@gmail.com>
2025-11-24 16:26:39 +0100

    Foundation?

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ A single header compression library for C++ 11
 Code style:
 namespace: snake_case
 functions: snake_case
-variables: camelCase
+variables: snake_case
 Classes/structs: UpperCase
--- tiny_bit.h
+++ tiny_bit.h
@@ -1,6 +1,54 @@
+#pragma once
+
+
 namespace tiny_bit {
 
+    void _encode(int &_data, int _offsetInBits, int _sizeInBits, int _value ) {
+        int mask = 0;
+        for (int i = 0; i < _sizeInBits; i++) {
+            mask |= (1 << i);
+        }
+        mask = mask << _offsetInBits;
+        _value = _value << _offsetInBits;
+        _data &= (~mask);
+        _data |= _value;
+    }
 
+    static int _decode(int _data, int _offsetInBits, int _sizeInBits) {
+        int mask = 0;
+        for (int i = 0; i < _sizeInBits; i++) {
+            mask |= (1 << i);
+        }
+        mask = mask << _offsetInBits;
+        _data &= mask;
+        return _data >> _offsetInBits;
+    }
+
+
+    class Property {
+    public:
+
+    };
+
+    template<typename T, typename M>
+    class PropertyImpl {
+    public:
+
+    };
+
+    class RegisteredObject {
+    public:
+        template<typename T, typename M>
+        RegisteredObject& property(M T::* member, size_t size_in_bits) {
+
+            return *this;
+        }
+
+
+    private:
+
+
+    };
 
 
 }
