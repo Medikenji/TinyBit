cffefa5df21f8bd1a44bf6617d83a16bb31e3c8a
thatalloguy <allosander@gmail.com>
2025-11-25 10:45:50 +0100

    [+] Added foundation for the registry

--- tiny_bit.h
+++ tiny_bit.h
@@ -1,4 +1,13 @@
-#pragma once
+#ifndef TINY_BIT_IMPLEMENTATION
+#define TINY_BIT_IMPLEMENTATION
+
+
+#include <unordered_map>
+
+/*
+ * #define DEBUG_OPTIONS
+ * Adds the print_tree function that prints out all of the stored data,
+ */
 
 
 namespace tiny_bit {
@@ -25,30 +34,102 @@ namespace tiny_bit {
     }
 
 
-    class Property {
-    public:
 
+    struct Property {
+        int size : 8;
+        int offset : 8;
+        size_t parent_hash : 8;
     };
 
-    template<typename T, typename M>
-    class PropertyImpl {
-    public:
 
-    };
 
     class RegisteredObject {
     public:
-        template<typename T, typename M>
-        RegisteredObject& property(M T::* member, size_t size_in_bits) {
 
+        template<typename T, typename M>
+        RegisteredObject& property(M T::* member, int size_in_bits) {
+            _properties.push_back(Property{
+                size_in_bits,
+                get_offset(),
+                typeid(T).hash_code()
+            });
             return *this;
         }
 
+        const std::vector<Property>& properties() const {
+            return _properties;
+        }
 
     private:
 
 
+        int get_offset() const {
+            int offset = 0;
+
+            for (int i=0; i<_properties.size(); i++) {
+                offset += _properties[i].size;
+            }
+
+            return offset;
+        }
+
+        int get_size() const {
+            int size = 0;
+
+            for (int i=0; i<_properties.size(); i++) {
+                size += _properties[i].size;
+            }
+
+            return size;
+        }
+
+        std::vector<Property> _properties;
     };
 
 
+    static std::unordered_map<size_t, RegisteredObject*> __registry;
+    typedef std::unordered_map<size_t, RegisteredObject*>::iterator __iterator;
+
+    template<typename T>
+    static RegisteredObject& register_object() {
+        size_t hash = typeid(T).hash_code();
+
+        __iterator it = __registry.find(hash);
+        if (it != __registry.end()) {
+            return *it->second;
+        }
+
+        RegisteredObject* registered_object = new RegisteredObject{};
+
+        __registry.insert({hash, registered_object});
+
+        return *registered_object;
+    }
+
+    static void clean_up() {
+        for (std::pair<const size_t, RegisteredObject*>& pair : __registry) {
+            delete pair.second;
+        }
+    }
+
+#ifdef DEBUG_OPTIONS
+
+    static void print_tree() {
+        for (std::pair<const size_t, RegisteredObject*>& pair : __registry) {
+
+            std::cout << "registered type: " << pair.first << "\n";
+
+            for (const Property& property : pair.second->properties()) {
+                std::cout << " - registered property:\n";
+                std::cout << " -- property.size: " << property.size << "\n";
+                std::cout << " -- property.offset: " << property.offset << "\n";
+            }
+        }
+    }
+#endif
+
+
+
 }
+
+#endif
